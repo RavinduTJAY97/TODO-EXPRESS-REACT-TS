bdbde8ec832e718999285fc86916840d83e7917b
Ravindu Jayasingha <ravindu.jayasingha@arimaclanka.com>
2024-08-16 18:23:35 +0530

    Add async thunks for adding and deleting todos

--- client/src/redux/tasksSlice.ts
+++ client/src/redux/tasksSlice.ts
@@ -13,6 +13,27 @@ export const fetchTodos = createAsyncThunk<Todo[]>(
   }
 );
 
+// Add new todo
+export const addTodoAsync = createAsyncThunk<Todo, { title: string }>(
+  "todos/addTodo",
+  async ({ title }) => {
+    const response = await axios.post("http://localhost:5000/tasks", {
+      title,
+      completed: false,
+    });
+    return response.data;
+  }
+);
+
+// Delete a todo
+export const deleteTodoAsync = createAsyncThunk<number, number>(
+  "todos/deleteTodo",
+  async (id) => {
+    await axios.delete(`http://localhost:5000/tasks/${id}`);
+    return id;
+  }
+);
+
 const initialState: TodoState = {
   items: [{ id: 1, title: "Create todo list", done: true }],
   loading: false,
